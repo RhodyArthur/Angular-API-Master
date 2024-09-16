5086162120ccba2b53c59a32327552ed594689cf
IT Compliance <rhodaarthur48@gmail.com>
2024-09-15 22:22:50 -0700

    Update api services

--- src/app/services/api-client-service.service.ts
+++ src/app/services/api-client-service.service.ts
@@ -1,21 +1,25 @@
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, Observable, retry, throwError } from 'rxjs';
+import { BehaviorSubject, catchError, Observable, retry, tap, throwError } from 'rxjs';
 import { Data } from '../interface/data';
 import { ErrorHandlingService } from './error-handling.service';
 import { environment } from '../../environments/environment.dev';
+import { StorageService } from './storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiClientServiceService {
 
+  // api url
   private jsonUrl = environment.apiUrl;
 
   // inject httpClient Service
   constructor(private http: HttpClient,
-              private errorHandler: ErrorHandlingService
+              private errorHandler: ErrorHandlingService,
+              private storageService: StorageService
   ) { }
+
 
   // fetching all posts
   getPosts(page:number = 1, pageSize:number = 10): Observable<Data[]> {
@@ -35,7 +39,7 @@ export class ApiClientServiceService {
 
   // retrieve a single post by id
   getPostById(id:number): Observable<Data> {
-    return this.http.get<Data>(`${this.jsonUrl}${id}`)
+    return this.http.get<Data>(`${this.jsonUrl}/${id}`)
     .pipe(
       retry(2),
       catchError(err => this.errorHandler.handleError(err))
@@ -48,7 +52,8 @@ export class ApiClientServiceService {
     return this.http.post<Data>(this.jsonUrl, body)
     .pipe(
       retry(2),
-      catchError(err => this.errorHandler.handleError(err))
+      catchError(err => this.errorHandler.handleError(err)),
+      tap(newPost => this.storageService.addPostToLocalStorage(newPost))
     );
   }
 
@@ -58,7 +63,8 @@ export class ApiClientServiceService {
     return this.http.put<Data>(`${this.jsonUrl}/${body.id}`, body)
     .pipe(
       retry(2),
-      catchError(err => this.errorHandler.handleError(err))
+      catchError(err => this.errorHandler.handleError(err)),
+      tap(updatedPost => this.storageService.updatePostInLocalStorage(updatedPost))
     );
   }
 
@@ -67,7 +73,8 @@ export class ApiClientServiceService {
     return this.http.delete<void>(`${this.jsonUrl}/${id}`)
     .pipe(
       retry(2),
-      catchError(err => this.errorHandler.handleError(err))
+      catchError(err => this.errorHandler.handleError(err)),
+      tap(() => this.storageService.removePostFromLocalStorage(id))
     );
   }
 
---
+++ src/app/services/storage.service.ts
@@ -0,0 +1,77 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, retry, catchError } from 'rxjs';
+import { environment } from '../../environments/environment.dev';
+import { Data } from '../interface/data';
+import { HttpClient } from '@angular/common/http';
+import { ErrorHandlingService } from './error-handling.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StorageService {
+    // api url
+    private jsonUrl = environment.apiUrl;
+    
+    // for state management
+    private postsSubject = new BehaviorSubject<Data[]>([]);
+    post$ = this.postsSubject.asObservable();
+    private localStorageKey = 'posts';
+
+  constructor(private http: HttpClient,
+              private errorHandler: ErrorHandlingService
+  ) { 
+    this.loadPostsFromLocalStorage();
+    this.loadPostsFromApi();
+  }
+
+  // load posts from local storage
+  loadPostsFromLocalStorage() {
+    const posts = localStorage.getItem(this.localStorageKey);
+    if(posts) {
+      this.postsSubject.next(JSON.parse(posts));
+    }
+  }
+
+  // load posts from API
+  loadPostsFromApi() {
+    this.http.get<Data[]>(this.jsonUrl)
+    .pipe(
+      retry(2),
+      catchError(err => this.errorHandler.handleError(err))
+    )
+    .subscribe(posts => {
+      this.postsSubject.next(posts);
+      this.savePostsToLocalStorage(posts);
+    })
+  }
+
+  savePostsToLocalStorage(posts: Data[]) {
+    localStorage.setItem(this.localStorageKey, JSON.stringify(posts));
+  }
+
+  addPostToLocalStorage(newPost: Data): void {
+    const currentPosts = this.postsSubject.value;
+    currentPosts.push(newPost);
+    this.postsSubject.next(currentPosts);
+    this.savePostsToLocalStorage(currentPosts);
+  }
+
+  updatePostInLocalStorage(updatedPost: Data): void {
+    const currentPosts = this.postsSubject.value;
+    const index = currentPosts.findIndex(p => p.id === updatedPost.id);
+    if (index > -1) {
+      currentPosts[index] = updatedPost;
+      this.postsSubject.next(currentPosts);
+      this.savePostsToLocalStorage(currentPosts);
+    }
+
+  }
+
+
+  removePostFromLocalStorage(id: number): void {
+    const currentPosts = this.postsSubject.value;
+    const updatedPosts = currentPosts.filter(post => post.id !== id);
+    this.postsSubject.next(updatedPosts);
+    this.savePostsToLocalStorage(updatedPosts);
+  }
+}
